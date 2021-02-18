309d77221e3361a81785f9c778c315949c417fd1
Mohammad Sholeh <mohammad.sholeh@gg-foods.com>
2021-02-18 17:40:12 +0700

    update trans index service

--- in_trans.php
+++ in_trans.php
@@ -6,11 +6,11 @@
                 if($trim =="PR"){
                     $fh = fopen('../interface/Backup/'.$file,'r');
                     $linecount=0;
-                    $linecount1=0;
-                    $linecountgagal=0;
+                    $id_history=0;
+                    // $linecountgagal=0;
 
                     while ($line = fgets($fh)) {
-                        if(strlen($line)>70) {
+                        if(strlen($line)>228) {
                             $cn = mysqli_connect("localhost","u1076725_ms","moha11mmad","u1076725_visit-pir");
                             $indnr=substr($line,0,8);
                             $lifnr=substr($line,8,18);
@@ -26,14 +26,15 @@
                             $bredesc=substr($line,173,198);
                             $catdesc=substr($line,198,228);
                             $status_pir=substr($line,228,230);
-                            $id_history = 0;
-                            $Chostory = "SELECT max(id_history)as mtid FROM trans_index";
-                            $exChstry = mysqli_query($cn,$Chostory);
-                            while($row =$exChstry-> fetch_assoc()){
-                                $id_history = $row['mtid']+1; 
+                            if($id_history==0){
+                                $Chostory = "SELECT max(id_history)as mtid FROM trans_index";
+                                $exChstry = mysqli_query($cn,$Chostory);
+                                while($row =$exChstry-> fetch_assoc()){
+                                    $id_history = $row['mtid']+1; 
+                                }
                             }
                             
-                            $sql = "INSERT INTO trans_index(id_history,indnr,lifnr,name1,prtype,regdate,desa,veraa_user,beastid,vistgid,lotid,sexdesc,bredesc,catdesc,status_pir)
+                            $sql = "INSERT INTO trans_index1(id_history,indnr,lifnr,name1,prtype,regdate,desa,veraa_user,beastid,vistgid,lotid,sexdesc,bredesc,catdesc,status_pir)
                                     values ($id_history,'$indnr','$lifnr','$name1','$prtype','$regdate','$desa','$veraa_user','$beastid','$vistgid','$lotid','$sexdesc','$bredesc','$catdesc','$status_pir')";
                             $result = mysqli_query($cn,$sql);
                             
@@ -42,6 +43,13 @@
                             }  
                         } 
                     }
+
+                    // $cn1 = mysqli_connect("localhost","u1076725_ms","moha11mmad","u1076725_visit-pir");
+                    // $Chostory = "SELECT max(id_history)as mtid FROM trans_index";
+                    // $exChstry = mysqli_query($cn1,$Chostory);
+                    // while($row =$exChstry-> fetch_assoc()){
+                    //     $id_history = $row['mtid']+1; 
+                    // }
 
                     if(!$linecount==0){
                         echo "success";
