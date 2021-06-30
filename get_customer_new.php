72b22f7fae238f417acd47eda98ff56006a09a05
Mohammad Sholeh <mohammad.sholeh@gg-foods.com>
2021-06-30 14:33:32 +0700

    get customer all

--- get_customer_new.php
+++ get_customer_new.php
@@ -2,10 +2,9 @@
 $link = mysqli_connect("localhost", "u1076725_ms", "moha11mmad", "u1076725_visit-pir-dev");
 // $link = mysqli_connect("localhost", "root", "", "absen_android");
 // if($_GET['id_wilayah']) { 
-	$name1=$_GET['id_user'];
-		$query=mysqli_query($link,"SELECT dtf.* FROM(SELECT * FROM(SELECT lifnr,name1,desa,veraa_user,indnr FROM trans_index WHERE id_history in(SELECT MAX(id_history) AS maxid 
-				FROM trans_index) GROUP BY indnr) as dt1 UNION ALL SELECT lifnr,name1,desa,veraa_user,indnr FROM trans_indexp) as dtf
-				JOIN mst_user mu ON mu.nama_karyawan=dtf.veraa_user WHERE id_user='$name1'");
+	// $name1=$_GET['id_user'];
+		$query=mysqli_query($link,"SELECT * FROM(SELECT lifnr,name1,desa,veraa_user,indnr FROM trans_index WHERE id_history in(SELECT MAX(id_history) AS maxid 
+				FROM trans_index) GROUP BY indnr) as dt1 UNION ALL SELECT lifnr,name1,desa,veraa_user,indnr FROM trans_indexp");
 	if (!$query) {
     	die(mysql_error());
 	}
@@ -15,9 +14,8 @@ $link = mysqli_connect("localhost", "u1076725_ms", "moha11mmad", "u1076725_visit
 		$response = array('error' => 'True');
 		echo json_encode($response);
 	}else {
-		$result = mysqli_query($link,"SELECT dtf.* FROM(SELECT * FROM(SELECT lifnr,name1,desa,veraa_user,indnr FROM trans_index WHERE id_history in(SELECT MAX(id_history) AS maxid 
-		FROM trans_index) GROUP BY indnr) as dt1 UNION ALL SELECT lifnr,name1,desa,veraa_user,indnr FROM trans_indexp) as dtf
-		JOIN mst_user mu ON mu.nama_karyawan=dtf.veraa_user WHERE id_user='$name1'");
+		$result = mysqli_query($link,"SELECT * FROM(SELECT lifnr,name1,desa,veraa_user,indnr FROM trans_index WHERE id_history in(SELECT MAX(id_history) AS maxid 
+		FROM trans_index) GROUP BY indnr) as dt1 UNION ALL SELECT lifnr,name1,desa,veraa_user,indnr FROM trans_indexp");
 	if (!$result) {
     die(mysql_error());
 	}
