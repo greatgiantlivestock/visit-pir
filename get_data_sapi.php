c68b45fa9fd0e9568e9c82ca9040c7f306fb6dc5
Mohammad Sholeh <mohammad.sholeh@gg-foods.com>
2021-06-30 09:43:42 +0700

    update query

--- get_data_sapi.php
+++ get_data_sapi.php
@@ -1,8 +1,7 @@
 <?php
 $link = mysqli_connect("localhost", "u1076725_ms", "moha11mmad", "u1076725_visit-pir-dev");
-	$query=mysqli_query($link,"SELECT ti.id,ti.indnr,ti.lifnr,ti.beastid,ti.vistgid 
-					FROM trans_index ti JOIN(SELECT MAX(id_history)AS id_history,lifnr FROM trans_index 
-					GROUP BY lifnr)AS dataMax ON dataMax.id_history=ti.id_history AND dataMax.lifnr=ti.lifnr");
+	$query=mysqli_query($link,"SELECT id,indnr,lifnr,beastid,vistgid 
+					FROM trans_index WHERE id_history IN(SELECT max(id_history) FROM trans_index) GROUP BY indnr");
 	if (!$query) {
     	die(mysql_error());
 	}
@@ -12,9 +11,8 @@ $link = mysqli_connect("localhost", "u1076725_ms", "moha11mmad", "u1076725_visit
 		$response = array('error' => 'True');
 		echo json_encode($response);
 	}else {
-		$result = mysqli_query($link,"SELECT ti.id,ti.indnr,ti.lifnr,ti.beastid,ti.vistgid 
-					FROM trans_index ti JOIN(SELECT MAX(id_history)AS id_history,lifnr FROM trans_index 
-					GROUP BY lifnr)AS dataMax ON dataMax.id_history=ti.id_history AND dataMax.lifnr=ti.lifnr");
+		$result = mysqli_query($link,"SELECT id,indnr,lifnr,beastid,vistgid 
+		FROM trans_index WHERE id_history IN(SELECT max(id_history) FROM trans_index) GROUP BY indnr");
 	if (!$result) {
     die(mysql_error());
 	}
--- get_pakan.php
+++ get_pakan.php
@@ -1,7 +1,7 @@
 <?php
 	$name1 = $_GET['id_user'];
 	$link = mysqli_connect("localhost", "u1076725_ms", "moha11mmad", "u1076725_visit-pir-dev");
-	$query=mysqli_query($link,"SELECT mp.* FROM mst_pakan mp JOIN (SELECT MAX(id_history) AS maxid, indnr,lifnr,veraa_user FROM trans_index GROUP BY lifnr) AS dt1 ON dt1.indnr=mp.indnr JOIN mst_user mu ON mu.nama_karyawan=dt1.veraa_user WHERE id_user='$name1'");
+	$query=mysqli_query($link,"SELECT mp.* FROM (SELECT * FROM mst_pakan where created_date in(select max(created_date) FROM mst_pakan))as mp JOIN (SELECT indnr,veraa_user FROM trans_index WHERE id_history IN(SELECT max(id_history) FROM trans_index)group by indnr) AS dt1 ON dt1.indnr=mp.indnr JOIN mst_user mu ON mu.nama_karyawan=dt1.veraa_user WHERE id_user='$name1'");
 	if (!$query) {
     	die(mysql_error());
 	}
@@ -11,7 +11,7 @@
 		$response = array('error' => 'True');
 		echo json_encode($response);
 	}else {
-		$result = mysqli_query($link,"SELECT mp.* FROM mst_pakan mp JOIN (SELECT MAX(id_history) AS maxid, indnr,lifnr,veraa_user FROM trans_index GROUP BY lifnr) AS dt1 ON dt1.indnr=mp.indnr JOIN mst_user mu ON mu.nama_karyawan=dt1.veraa_user WHERE id_user='$name1'");
+		$result = mysqli_query($link,"SELECT mp.* FROM (SELECT * FROM mst_pakan where created_date in(select max(created_date) FROM mst_pakan))as mp JOIN (SELECT indnr,veraa_user FROM trans_index WHERE id_history IN(SELECT max(id_history) FROM trans_index)group by indnr) AS dt1 ON dt1.indnr=mp.indnr JOIN mst_user mu ON mu.nama_karyawan=dt1.veraa_user WHERE id_user='$name1'");
 		if (!$result) {
     		die(mysql_error());
 		}
