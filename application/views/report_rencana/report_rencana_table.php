c30e2572b406ea150c8e27774ea40e7eedc3f50f
Mohammad Sholeh <mohammad.sholeh@gg-foods.com>
2021-07-07 11:55:56 +0700

    dev check rencana

--- application/views/report_rencana/report_rencana_table.php
+++ application/views/report_rencana/report_rencana_table.php
@@ -16,9 +16,6 @@
 	});
 </script>
 	<div class="widget-box widget-color-nurmal" id="widget-box-9">
-		<!-- <div class="widget-header">
-			<h5 class="widget-title"><i class="fa fa-tags"> </i> <?php echo $judul; ?></h5>
-		</div> -->
 		<div class="widget-body">
 		<form class="form-horizontal"  action="<?php echo base_url(); ?>Report_rencana/lihat_report" method="post"/>
 			<input type="hidden" name="tanggal1" value="<?php echo $tanggal_mulai; ?>">
@@ -80,14 +77,6 @@
 				<div class="row">
 					<div class="col-md-12">
 					<div class="col-md-12 text-center">
-						<!-- <a class="btn btn-xs btn-success text-center" 
-							href="<?php echo base_url().'report_rencana/cetak/'.$tanggal_mulai.'/'.
-							$tanggal_sampai.'/'.$nama_karyawan; ?>" <?php echo $disable; ?> 
-							onclick="window.open(this.href, 'newwindow', 'width=600, height=470'); 
-							return false;">
-							<i class="ace-icon fa fa-print"></i>
-							<span class="bigger-110">Print</span>
-						</a> -->
 						<a class="btn btn-xs btn-primary text-center"
 							href="javascript://" onclick="exportRealisasiKunjungan('xls');"
 							<?php echo $disable; ?>>
@@ -97,16 +86,6 @@
 					</div>
 						<table id="dataTables-example" class="table table-striped table-bordered table-hover">
 							<thead>
-								<!-- <tr>
-									<th style="background: #22313F;color:#fff;">PPL</th>																	
-									<th style="background: #22313F;color:#fff;">Petani</th>									
-									<th style="background: #22313F;color:#fff;">Alamat</th>									
-									<th style="background: #22313F;color:#fff;">Tanggal Rencana</th>
-									<th style="background: #22313F;color:#fff;">Waktu Checkin</th>
-									<th style="background: #22313F;color:#fff;">Waktu Checkout</th>
-									<th style="background: #22313F;color:#fff;">Durasi Kunjungan</th>
-									<th style="background: #22313F;color:#fff;">Status</th>
-								</tr>  -->
 								<tr>
 									<th style="background: #22313F;color:#fff;">Index</th>																	
 									<th style="background: #22313F;color:#fff;">Petani</th>									
@@ -130,17 +109,6 @@
 																	AND nama LIKE '%$nama_karyawan' AND id_aplikasi='2'
 																	ORDER BY trm.nomor_rencana ASC");
 								}else if($this->session->userdata("id_role") == 3 || $this->session->userdata("id_role") == 4 || $this->session->userdata("id_role") == 6){
-									// $q_tarik_data = $this->db->query("SELECT trm.nomor_rencana,trm.tanggal_penetapan,trm.tanggal_rencana,
-									// 								trm.keterangan, trs.name1,trs.desa,tanggal_checkout,tanggal_checkin,mu.nama,trd.status_rencana
-									// 								FROM trx_rencana_master trm JOIN trx_rencana_detail trd
-									// 								ON trm.id_rencana_header = trd.id_rencana_header 
-									// 								JOIN trans_index trs ON trs.lifnr = trd.id_customer
-									// 								JOIN mst_user mu ON mu.id_karyawan = trd.id_karyawan
-									// 								LEFT JOIN trx_checkin ON trx_checkin.id_rencana_detail=trd.id_rencana_detail
-									// 								LEFT JOIN trx_checkout ON trx_checkout.id_rencana_detail=trd.id_rencana_detail
-									// 								WHERE trm.tanggal_rencana BETWEEN '$tanggal_mulai' AND '$tanggal_sampai'
-									// 								AND nama LIKE '%$nama_karyawan%' AND id_aplikasi='2' GROUP BY trd.id_rencana_detail
-									// 								ORDER BY trm.nomor_rencana ASC");
 									$q_tarik_data = $this->db->query("SELECT id_rencana_detail,indnr,name1,desa,SUM(a) AS durasi,COUNT(id_rencana_detail)AS jml FROM
 																	(SELECT trd.id_rencana_detail,trd.indnr,name1,desa,TIMESTAMPDIFF(SECOND, tanggal_checkin,tanggal_checkout)AS a 
 																	FROM trx_rencana_master trm JOIN trx_rencana_detail trd 
@@ -159,7 +127,7 @@
 																	ON trm.id_rencana_header = trd.id_rencana_header 
 																	JOIN mst_customer mcs ON mcs.id_customer = trd.id_customer
 																	JOIN mst_user mu ON mu.id_karyawan = trd.id_karyawan
-																	WHERE mu.id_departemen = '$departemen' AND trm.tanggal_rencana BETWEEN '$tanggal_mulai' AND '$tanggal_sampai'
+																	WHERE trm.tanggal_rencana BETWEEN '$tanggal_mulai' AND '$tanggal_sampai'
 																	AND mu.id_karyawan ='$id_karyawan' AND id_aplikasi='2'
 																	ORDER BY trm.nomor_rencana ASC");
 								}else{
@@ -191,60 +159,6 @@
 			</div>
 		</div>
 	</div>
- <!-- <div class="modal fade" id="ModalInputKegiatan" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-header" style="margin-bottom: 10px;">
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                <h4 class="modal-title" id="myModalLabel">Detail Rencana</h4>
-            </div>
-            <div class="modal-body">
-			<form>
-				<table id="sample-table-2" class="table table-striped table-bordered table-hover">
-					<thead>
-						<tr>
-							<th>Nama Karyawan</th>	
-							<th>Kegiatan</th>
-							<th>Customer</th>
-							<th>Status</th>
-						</tr> 
-					</thead>
-					<tbody>
-						<?php
-						$id='38';
-						?>
-						<input id="nama_kegiatan" type="text" name="keterangan">
-						<?php 
-						$rcn_dt = $this->db->query("SELECT trx_rencana_detail.id_rencana_detail,
-													trx_rencana_detail.id_rencana_header,trx_rencana_detail.status_rencana,
-													mst_kegiatan.nama_kegiatan,mst_customer.nama_customer, 
-													trx_rencana_detail.id_karyawan, tk_personal.nama_karyawan 
-													FROM trx_rencana_detail JOIN tk_personal 
-													ON tk_personal.id_karyawan = trx_rencana_detail.id_karyawan 
-													JOIN mst_kegiatan ON trx_rencana_detail.id_kegiatan=mst_kegiatan.id_kegiatan
-													JOIN mst_customer ON trx_rencana_detail.id_customer=mst_customer.id_customer
-													WHERE trx_rencana_detail.id_rencana_header= '$id'
-													ORDER BY trx_rencana_detail.id_rencana_detail DESC"); 
-						$no = 1;
-						foreach($rcn_dt->result_array() as $data) { ?>	
-							<tr>
-								<td><?php echo $data['nama_karyawan']; ?></td>
-								<td><?php echo "Belum Terealisasi"; ?></td>
-								<td><?php echo "Belum Terealisasi"; ?></td>
-								<td><?php echo "Belum Terealisasi"; ?> </td>
-							</tr>
-						<?php 
-						$no++; } ?>	
-					</tbody>							
-				</table>
-			</form>
-	        <div class="modal-footer">	
-			    <button type="button" class="btn btn-danger btn-sm" data-dismiss="modal">Close</button>
-			</div>
-			</div>  
-        </div>
-    </div>
-</div> -->
 <?php 
 	if($this->session->userdata("error_duplicate") == true) { 
 		$this->session->unset_userdata('error_duplicate');
