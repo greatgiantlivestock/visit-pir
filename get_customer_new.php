c638f7a20df013ffea413d5c650407c2b08f2442
Mohammad Sholeh <mohammad.sholeh@gg-foods.com>
2021-07-21 08:31:06 +0700

    update API customer

--- get_customer_new.php
+++ get_customer_new.php
@@ -3,7 +3,7 @@ $link = mysqli_connect("localhost", "u1076725_ms", "moha11mmad", "u1076725_visit
 // $link = mysqli_connect("localhost", "root", "", "absen_android");
 // if($_GET['id_wilayah']) { 
 	// $name1=$_GET['id_user'];
-		$query=mysqli_query($link,"SELECT * FROM(SELECT lifnr,name1,desa,veraa_user,indnr FROM trans_index WHERE id_history in(SELECT MAX(id_history) AS maxid 
+		$query=mysqli_query($link,"SELECT * FROM(SELECT lifnr,name1,desa,veraa_user,indnr,regdate FROM trans_index WHERE id_history in(SELECT MAX(id_history) AS maxid 
 				FROM trans_index) GROUP BY indnr) as dt1 UNION ALL SELECT lifnr,name1,desa,veraa_user,indnr FROM trans_indexp");
 	if (!$query) {
     	die(mysql_error());
@@ -14,7 +14,7 @@ $link = mysqli_connect("localhost", "u1076725_ms", "moha11mmad", "u1076725_visit
 		$response = array('error' => 'True');
 		echo json_encode($response);
 	}else {
-		$result = mysqli_query($link,"SELECT * FROM(SELECT lifnr,name1,desa,veraa_user,indnr FROM trans_index WHERE id_history in(SELECT MAX(id_history) AS maxid 
+		$result = mysqli_query($link,"SELECT * FROM(SELECT lifnr,name1,desa,veraa_user,indnr,regdate FROM trans_index WHERE id_history in(SELECT MAX(id_history) AS maxid 
 		FROM trans_index) GROUP BY indnr) as dt1 UNION ALL SELECT lifnr,name1,desa,veraa_user,indnr FROM trans_indexp");
 		if (!$result) {
 			die(mysql_error());
