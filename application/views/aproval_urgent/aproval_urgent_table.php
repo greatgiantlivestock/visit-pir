9af020eb259e33799f493d2b360f5e5f2580f8dd
Mohammad Sholeh <mohammad.sholeh@gg-foods.com>
2021-07-25 09:16:26 +0700

    remove plant group

--- application/views/aproval_urgent/aproval_urgent_table.php
+++ application/views/aproval_urgent/aproval_urgent_table.php
@@ -47,7 +47,7 @@
 								$no = 1;
 										foreach($customer->result_array() as $data) { ?>
 										<tr>
-											<td><?php echo $data['veraa_user']; ?></td>
+											<td><?php echo $data['nama']; ?></td>
 											<td><?php echo $data['tanggal_rencana']; ?></td>
 											<td><?php echo $data['tanggal_penetapan']; ?></td>
 											<td><?php echo $data['jml']; echo " Petani"; ?></td>
