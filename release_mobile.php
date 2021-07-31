7d17d0c7001b2b35dc6a5f61f8440fc82cc36416
Mohammad Sholeh <mohammad.sholeh@gg-foods.com>
2021-07-31 12:02:45 +0700

    release Product

--- release_mobile.php
+++ release_mobile.php
@@ -5,20 +5,21 @@
     while ($rec =$queryNR-> fetch_assoc()) {
         $noRelease = $rec['jml']+1;
     }
-	$date = date("Ymd");
-    $query=mysqli_query($link,"SELECT id_trx_pengobatan,indnr,kode_obat,qty,DATE(tanggal)as tanggal FROM trx_pengobatan tp JOIN trx_rencana_detail trd 
-                        ON tp.id_rencana_detail=trd.id_rencana_detail WHERE status_release=0");
+	$date = date("Ymd_His");
+    $query=mysqli_query($link,"SELECT id_trx_pengobatan,indnr,kode_obat,qty,DATE(tanggal)as tanggal,unit_obat FROM trx_pengobatan tp JOIN trx_rencana_detail trd 
+                        ON tp.id_rencana_detail=trd.id_rencana_detail JOIN mst_obat mo on tp.kode_obat=mo.kode_obat WHERE status_release=0");
     
     $sukses = 0;
     $gagal = 0;
     while ($rec1 =$query-> fetch_assoc()) {
-        $fp = fopen("../interface/To/RP_".$noRelease."_".$date.".txt","a") or die("Unable to open file!");
-		$fp1 = fopen("../interface/To_backup/RP_".$noRelease."_".$date.".txt","a") or die("Unable to open file!");
+        $fp = fopen("../interface/To/RP_".$date.".txt","a") or die("Unable to open file!");
+		$fp1 = fopen("../interface/To_backup/RP_".$date.".txt","a") or die("Unable to open file!");
 		$data1 = $rec1['indnr'];
 		$data2 = $rec1['kode_obat'];
 		$data3a = strpos($rec1['qty'],'.');
 		$data3b = strpos($rec1['qty'],',');
 		$data4 = str_replace("-","",$rec1['tanggal']);
+		$data5 = $rec1['unit_obat'].'   ';
 		if($data3a==''){
 			if($data3b==''){
 				$data3 = '000000000000000'.$rec1['qty'].'00';
@@ -28,7 +29,7 @@
 		}else{
 			$data3 = '000000000000000'.number_format((float)$rec1['qty'], 2, '', '');
 		}
-		$content = $data1.$data2.substr($data3,-8).$data4."\n";
+		$content = $data1.$data2.substr($data3,-8).substr($data5,0,3).$data4."\n";
 		if($data1 != ''){
 			fwrite($fp,$content);
 			fclose($fp);
